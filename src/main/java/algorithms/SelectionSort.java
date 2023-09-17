08e99898ef3cdf407a8b05738032cdf4d5fed369
rojojun <zoloman316@gmail.com>
2023-09-17 18:32:41 +0900

    Study of 자료구조 (5)

---
+++ src/main/java/algorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+public class SelectionSort {
+    // 배열 i와 j의 위치를 바꿈
+    public static void swapElements(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static int indexLowest(int[] array, int start) {
+        int lowIndex = start;
+        for (int i = start; i < array.length; i++) {
+            if (array[i] < array[lowIndex]) {
+                lowIndex = i;
+            }
+        }
+        return lowIndex;
+    }
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int j = indexLowest(array, i);
+            swapElements(array, i, j);
+        }
+    }
+}
